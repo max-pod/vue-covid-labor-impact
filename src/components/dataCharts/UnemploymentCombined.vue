04d7636f6b95452990232661f015dfbf311a1485
Maxim Podolski <podolsk_maxi@bentley.edu>
2020-09-23 23:06:32 -0400

    Simple Area Chart Failure

--- src/components/dataCharts/UnemploymentCombined.vue
+++ src/components/dataCharts/UnemploymentCombined.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <h1>Unemployment Combined</h1>
+        <UnemploymentAreaChart>weee</UnemploymentAreaChart>
     </div>
 </template>
 
 <script>
+import AreaChart from "../charts/AreaChart.vue";
 
 export default {
     name: 'UnemploymentCombined',
+    components: {
+        UnemploymentAreaChart: AreaChart
+    }
 }
